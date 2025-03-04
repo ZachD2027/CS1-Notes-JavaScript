03ddad25fb5ab4afdb405197c74ea0cabae6318c
Katerina Walter <94501766+katerinanavab@users.noreply.github.com>
2025-03-04 17:57:33 +0000

    4.1 notes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+/* This is a MULTI-LINE COMMENT
+The code below is an example STATEMENT
+(instruction/command) 
+Every STATEMENT ends in a SEMICOLON (;)
+ */
+console.log("Welcome to JavaScript!");
+
+// This is a SINGLE-LINE COMMENT
+
+// console.log() is a FUNCTION that displays info
+console.log("My name is Katerina");
+
+
+
+
+
