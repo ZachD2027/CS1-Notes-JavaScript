d7197110240f7ed1110ac84c0e76a8ace34db38e
ZachD2027 <zachary.dowling2027@gbwl.org>
2025-03-05 17:10:05 +0000

    done

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ console.log("Welcome to JavaScript!");
 // This is a SINGLE-LINE COMMENT
 
 // console.log() is a FUNCTION that displays info
-console.log("My name is Katerina");
+console.log("My name is Zach");
 
+// DECLARE a new variable (buying a box)
+let luckyNumber;
 
+// ASSIGN a value to that variable // (putting an item in a box)
+luckyNumber = 26;
 
+//DECLARE + ASSIGN in one statement
+let worstNumber = 21;
 
+//You can "PASS IN" a variable into a function
+console.log(luckyNumber);
 
